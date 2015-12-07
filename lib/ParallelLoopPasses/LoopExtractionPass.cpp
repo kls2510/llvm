8bdf3e0c2957a1bf77ebc4f01563dde084affb02
Katie Scott <kls82@cam.ac.uk>
2015-12-07 16:40:44 +0000

    fixing cast error

--- lib/ParallelLoopPasses/LoopExtractionPass.cpp
+++ lib/ParallelLoopPasses/LoopExtractionPass.cpp
@@ -192,7 +192,7 @@ namespace {
 								BasicBlock *writeTo = BasicBlock::Create(context, "loads", newLoopFunc);
 								builder.SetInsertPoint(writeTo);
 								cerr << "creating map\n";
-								for (auto i : args1) {
+								for (auto &i : args1) {
 									//load each struct element at the start of the function
 									Value *mapVal = builder.CreateStructGEP(myStruct, structArg, p);
 									LoadInst *loadInst = builder.CreateLoad(mapVal);
