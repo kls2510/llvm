ec635424ca691760258f3eedee7e84cc9d3b5b38
Katie Scott <kls82@cam.ac.uk>
2015-12-07 12:21:57 +0000

    changing order of instructions and adding all args to one struct

--- lib/ParallelLoopPasses/LoopExtractionPass.cpp
+++ lib/ParallelLoopPasses/LoopExtractionPass.cpp
@@ -58,92 +58,83 @@ namespace {
 						if (loopData->isParallelizable()) {
 							//extract the loop
 							cerr << "This loop has no dependencies so can be extracted\n";
-							//find no of iterations and the start iteration value
-							//Code for extension to not being canonicalised
-							/* Instruction *inst2 = (loopData->getLoop())->getHeader()->begin();
-							Value *noIterations;
-							while (loopData->getLoop()->contains(inst2)) {
-								cerr << (inst2->getValueName())->getKeyData() << "\n";
-								if (strcmp((inst2->getValueName())->getKeyData(),"exitcond") == 0) {
-									cout << "found the exit condition value\n";
-									int noOperands = inst2->getNumOperands();
-									noIterations = (inst2->getOperand(noOperands - 1));
-								}
-								inst2 = inst2->getNextNode();
-							} */
-							int noIterations = SE.getSmallConstantTripCount(loopData->getLoop());
-							Value *startIt = ((loopData->getLoop())->getCanonicalInductionVariable())->getIncomingValue(0);
 
 							//get pointer to the basic block we'll insert the new instructions into
 							BasicBlock *insertPos = ((loopData->getLoop())->getLoopPredecessor());
-
-							//create the struct we'll use to pass data to/from the threads
-							StructType *myStruct = StructType::create(context, "ThreadPasser");
-							//send the startItvalue, no of iterations, Total number of threads and thread number
-							//Type *elts[] = { startIt->getType(), noIterations->getType(), Type::getInt32Ty(context), Type::getInt32Ty(context) };
-							Type *elts[] = { startIt->getType(), Type::getInt32Ty(context), Type::getInt32Ty(context), Type::getInt32Ty(context) };
-							myStruct->setBody(elts);
-
-							//setup for inserting instructions before the loop
-							Instruction *inst = insertPos->begin();
-							IRBuilder<> builder(inst);
-							list<Value *> threadStructs;
-							for (int i = 0; i < noThreads; i++) {
-								//initialise struct in a new instruction at the end of the basic block
-								AllocaInst *allocateInst = builder.CreateAlloca(myStruct);
-								//store startIt
-								Value *getPTR = builder.CreateStructGEP(myStruct, allocateInst, 0);
-								builder.CreateStore(startIt, getPTR);
-								//store numIt
-								getPTR = builder.CreateStructGEP(myStruct, allocateInst, 1);
-								//builder.CreateStore(noIterations, getPTR);
-								builder.CreateStore(ConstantInt::get(Type::getInt32Ty(context), noIterations), getPTR);
-								//store total num threads
-								getPTR = builder.CreateStructGEP(myStruct, allocateInst, 2);
-								builder.CreateStore(ConstantInt::get(Type::getInt32Ty(context), noThreads), getPTR);
-								//store this one's thread number
-								getPTR = builder.CreateStructGEP(myStruct, allocateInst, 3);
-								builder.CreateStore(ConstantInt::get(Type::getInt32Ty(context), i), getPTR);
-								//load the struct for passing into the function
-								LoadInst *loadInst = builder.CreateLoad(allocateInst);
-								threadStructs.push_back(loadInst);
-							}
 
 							//extract the loop into a new function
 							CodeExtractor extractor = CodeExtractor(DT, *(loopData->getLoop()), false);
 							Function *extractedLoop = extractor.extractCodeRegion();
 
 							if (extractedLoop != 0) {
+								//for now - change when we include non-canonicalised loops
+								int noIterations = SE.getSmallConstantTripCount(loopData->getLoop());
+								Value *startIt = ((loopData->getLoop())->getCanonicalInductionVariable())->getIncomingValue(0);
+
+								//create the struct we'll use to pass data to/from the threads
+								StructType *myStruct = StructType::create(context, "ThreadPasser");
+								//send the: data required, startItvalue and endIt value
+								vector<Type *> elts;
+
+								//setup for inserting instructions before the loop
+								Instruction *inst = insertPos->begin();
+								IRBuilder<> builder(inst);
+								list<Value *> threadStructs;
+								Value *iterationsEach = builder.CreateExactSDiv(ConstantInt::get(Type::getInt32Ty(context), noIterations), ConstantInt::get(Type::getInt32Ty(context), noThreads));
+								Value *threadStartIt;
+								Value *endIt;
+								//setup struct type
 								CallInst *callInst = dyn_cast<CallInst>(*(extractedLoop->user_begin()));
 								int noOps = callInst->getNumArgOperands();
-								vector<Value *> args;
 								for (int i = 0; i < noOps; i++) {
-									args.push_back(callInst->getOperand(i));
+									elts.push_back(callInst->getOperand(i)->getType());
 								}
+								elts.push_back(threadStartIt->getType());
+								elts.push_back(endIt->getType());
+								myStruct->setBody(elts);
+
+								for (int i = 0; i < noThreads; i++) {
+									Value *startItMult = builder.CreateMul(iterationsEach, ConstantInt::get(Type::getInt32Ty(context), i));
+									Value *threadStartIt = builder.CreateAdd(startIt, startItMult);
+									if (i == (noThreads - 1)) {
+										endIt = builder.CreateAdd(threadStartIt, ConstantInt::get(Type::getInt32Ty(context), noIterations));
+									}
+									else {
+										endIt = builder.CreateAdd(threadStartIt, iterationsEach);
+									}
+
+									AllocaInst *allocateInst = builder.CreateAlloca(myStruct);
+									//store original arguments in struct
+									for (int i = 0; i < noOps; i++) {
+										Value *getPTR = builder.CreateStructGEP(myStruct, allocateInst, i);
+										builder.CreateStore(callInst->getOperand(i), getPTR);
+									}
+									//store startIt
+									Value *getPTR = builder.CreateStructGEP(myStruct, allocateInst, noOps + 0);
+									builder.CreateStore(threadStartIt, getPTR);
+									//store endIt
+									getPTR = builder.CreateStructGEP(myStruct, allocateInst, noOps + 1);
+									builder.CreateStore(endIt, getPTR);
+									//load the struct for passing into the function
+									LoadInst *loadInst = builder.CreateLoad(allocateInst);
+									threadStructs.push_back(loadInst);
+								}
+
 								IRBuilder<> callbuilder(callInst);
 
 								//create a new function with added argument types
 								Module * mod = (F.getParent());
-								vector<Type *> paramTypes;
-								for (int i = 0; i < (extractedLoop->getFunctionType())->getNumParams(); i++) {
-									paramTypes.push_back((extractedLoop->getFunctionType())->getParamType(i));
-								}
+								SmallVector<Type *, 8> paramTypes;
 								paramTypes.push_back(myStruct);
-								ArrayRef<Type *> types(paramTypes);
-								FunctionType *FT = FunctionType::get(extractedLoop->getFunctionType()->getReturnType(), types, false);
-								string name = (extractedLoop->getName()).str() + "_";
+								FunctionType *FT = FunctionType::get(extractedLoop->getFunctionType()->getReturnType(), paramTypes, false);
+								string name = "_" + (extractedLoop->getName()).str() + "_";
 								Function *newLoopFunc = Function::Create(FT, Function::ExternalLinkage, name, mod);
-								cerr << "New function created\n";
-								//Constant *c = mod->getOrInsertFunction(name, FT);
-								//Function *newLoopFunc = cast<Function>(c);
 
-								//insert calls to this new function
+								//insert calls to this new function - for now just call function, add threads later
 								for (list<Value*>::iterator it = threadStructs.begin(); it != threadStructs.end(); ++it) {
-									vector<Value *> argsForCall = args;
+									SmallVector<Value *,8> argsForCall;
 									argsForCall.push_back(*it);
-									ArrayRef<Value *> args(argsForCall);
-									callbuilder.CreateCall(newLoopFunc, args);
-									cerr << "New function call created\n";
+									callbuilder.CreateCall(newLoopFunc, argsForCall);
 								}
 
 								//delete the original call instruction
@@ -153,13 +144,17 @@ namespace {
 								ValueToValueMapTy vvmap;
 								Function::ArgumentListType &args1 = extractedLoop->getArgumentList();
 								Function::ArgumentListType &args2 = newLoopFunc->getArgumentList();
-								Argument *arg = (args2.begin());
-								for (SymbolTableList<Argument>::iterator i = args1.begin(); i != args1.end(); ++i) {
-									vvmap.insert(pair<Value*, Value*>(cast<Value>(i), cast<Value>(arg)));
-									arg = args2.getNext(arg);
+								StructType *structArg = dyn_cast<StructType *>(args2);
+								vector<Type *> body = structArg->elements();
+								vector<Type *>::iterator it = body.begin();
+								for (auto &i : args1) {
+									vvmap.insert(std::make_pair(cast<Value>(&i), cast<Value>((*it))));
+									it++;
 								}
+								//need to return changed local values but for now return nothing
 								SmallVector<ReturnInst *, 0> returns;
 								CloneFunctionInto(newLoopFunc, extractedLoop, vvmap, false, returns, "");
+
 
 								//Debug
 								cerr << "Original function rewritten to:\n";
@@ -167,11 +162,9 @@ namespace {
 								cerr << "with new function:\n";
 								newLoopFunc->dump();
 								//Mark the function to avoid infinite extraction
-								//extractedLoop->removeFromParent();
 								newLoopFunc->addFnAttr("Extracted", "true");
 								extractedLoop->addFnAttr("Extracted", "true");
 								F.addFnAttr("Extracted", "true");
-								//extractedLoop->eraseFromParent();
 							}
 
 						}
